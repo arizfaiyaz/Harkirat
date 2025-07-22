06edb823d6d4af05d073476b589aeda281d8bc5a
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-07-23 00:17:35 +0530

    Add package.json for sumserver with express dependency

---
+++ sumserver/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.get('/sum', (req, res) => {
+    const { a, b } = req.query;
+    const sum = parseInt(a) + parseInt(b);
+    res.send(`The sum is ${sum}`);
+});
+
+app.get('/multiply', (req, res) => {
+    const { a, b } = req.query;
+    const product =  parseInt(a) * parseInt(b);
+    res.send(`The product is ${product}`);
+});
+
+app.get('/subtract', (req, res) => {
+    const { a, b } = req.query;
+    const difference = parseInt(a) - parseInt(b);
+    res.send(`The difference is ${difference}`);
+
+});
+
+app.get('/divide', (req, res) => {
+    const { a, b } = req.query;
+    const quotient = parseInt(a) / parseInt(b);
+    res.send(`The quotient is ${quotient}`);
+});
+
+app.listen(3000);
