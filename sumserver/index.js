9e4746086f9bc0bf4b44db748182e724792fee53
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-07-24 21:00:40 +0530

    feat: implement request logging middleware

--- sumserver/index.js
+++ sumserver/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 
 let requestCount = 0;
-
-
-
+function logRequest(req, res, next){
+    requestCount++;
+    console.log(`Request ${requestCount}: ${req.method}`);
+    next();
+}
+app.use(logRequest);
 app.get('/sum', (req, res) => {
     const { a, b } = req.query;
     const sum = parseInt(a) + parseInt(b);
