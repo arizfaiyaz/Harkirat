713eeed4bca066c4942cff3830415ed36492a6f5
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-07-25 23:37:47 +0530

    fix: correct response structure in sum endpoint

--- cors/index.js
+++ cors/index.js
@@ -3,5 +3,9 @@ const express = require('express');
 const app = express();
 
 app.post('/sum', (req, res) => {
-    
+    const a = parseInt(req.body.a);
+    const b = parseInt(req.body.b);
+    res.json({
+        answer: a + b
+    })
 })
