edea0afa82d8362eed00314d8cc9c8d60141c29b
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-08-16 00:32:45 +0530

    chore: initialize package.json for course-selling-app with dependencies

---
+++ Course-selling-app/indes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const jsonwebtoken = require('jsonwebtoken');
+const { z } = require('zod');
+
+const app = express();
+
+app.post('/signup', (req, res) => {
+
+});
+
+app.post('/signin', (req, res) => {
+
+});
+
+app.post('/signup', (req, res) => {
+
+});
+
+
+
+
+
+
+app.listen(3000);
