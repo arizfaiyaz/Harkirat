a26ceb48b2b0a5505ad70211d33ec77d890fc692
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-08-16 01:21:15 +0530

    feat: initialize express app and set up user and course routers

--- Course-selling-app/routes/user.js
+++ Course-selling-app/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const Router = express.Router();
+const userRouter = express.Router();
 
-const userRouter = Router();
+
 
 userRouter.post('/signup', (req, res) => {
 
