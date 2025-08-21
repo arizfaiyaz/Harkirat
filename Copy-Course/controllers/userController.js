abc20e7b03567c4181bf660700da187fd0fc5d7f
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-08-22 00:35:28 +0530

    feat: implement user signout and enhance getUserPurchases functionality with error handling

--- Copy-Course/controllers/userController.js
+++ Copy-Course/controllers/userController.js
@@ -82,3 +82,51 @@ async function userSignin(res, res) {
     }
 }
 
+// User signout
+async function userSignout(req, res) {
+    res.session.destroy((err) => {
+        if(err) {
+            return res.json({
+                message: "failed to sign out"
+            });
+        }
+        res.json({
+            message: "Signout successful"
+        });
+    });
+}
+
+// get user purchases
+async function getUserPurchases(req, res) {
+    const userId = req.session.userId;
+
+    if(!userId){
+        return res.json({
+            message: "Unauthorized acces",
+        });
+    }
+    const purchases = await purchaseModel.findOne({
+        userId,
+    });
+
+    if(!purchases.length){
+        return res.json({
+            message:"No purchases found";
+        });
+    }
+    const purchasesCourseIds = purchases.modifiedPaths(purchase => purchase.courseId);
+    const courseData = await courseModel.find({
+        _id: { $in: purchasesCourseIds}
+    })
+    res.json({
+        purchases,
+        courseData,
+    });
+}
+
+module.exports = {
+    userSignup,
+    userSignin,
+    userSignout,
+    getUserPurchases,
+};
