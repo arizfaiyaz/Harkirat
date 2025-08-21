8fbe891f48ddfb8d6c400334f6c44f039f310a93
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-08-21 19:32:07 +0530

    feat: create Admin schema model for database

---
+++ Copy-Course/models/adminModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+const AdminSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+    firstName: {
+        type: String,
+        required: true,
+
+    },
+    lastName: {
+        type: String,
+         required: true,
+
+    },
+
+});
