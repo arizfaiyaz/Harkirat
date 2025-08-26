f1367014a96561a9e653818c3347b8ed9f9e8dae
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-08-26 23:06:29 +0530

    feat: implement addTodos function to manage todo list

--- React/todo/src/App.jsx
+++ React/todo/src/App.jsx
@@ -2,11 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [todos, setTodos] = useState([{
+    title: "Learn React",
+    description: "study the basics of React",
+    done: false
+  }]);
+
+function addTodos(){
+  setTodos([...todos])
+};
 
   return (
     <>
-      
+      <div>
+        <button onClick={addTodos}>Add todo</button>
+      </div>
     </>
   )
 }
