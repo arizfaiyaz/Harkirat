55bec8947e1710db1b07ca40aaadce7de5f1ea09
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-10-15 23:25:10 +0530

    feat: restructure App component and remove unused styles

--- React/third/week-9-react/src/App.jsx
+++ React/third/week-9-react/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{background: "#dfe6e9", height: "100vh"}}>
+        <div style={{display: "flex", justifyContent: "center"}}>
+        <PostComponent />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
+const style = {
+  width: 200,
+  backgroundColor: "white",
+  borderRadius: 10,
+  borderColor: "gray",
+  borderWidth: 1.5,
+  padding: 20
+}
+
+function PostComponent() {
+  return (
+    <>
+    <div style={style}>
+    <div style={{display: "flex"}}>
+      <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjxhOxB-AhzpZ5M55ItVi7vEVt__zg0W4-hw&s"} style={{
+        width: 50,
+        height: 50,
+        borderRadius:40,
+        display: "flex"
+      }} />
+    </div>
+    <div style={{fontSize:10, margin: 5}}>
+      <b>
+        100xDevs
+      </b>
+      <div>23,888 followers</div>
+      <div>12M</div>
+    </div>
+    <div>
+        Want to know how to win big? Check out how these folks won $500 in bounties.
+    </div>
+    </div>
+    </>
+  )
+
+}
+
 export default App
+// structuring ur app into components
+// defining a state of your application
