853aaccd5d6802703f2889fa90f921eddd219fd0
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-01-28 01:10:55 +0530

    feat: implement LightBulb component with toggle functionality

--- React/fifth/src/App.jsx
+++ React/fifth/src/App.jsx
@@ -1,10 +1,47 @@
+import { useState } from "react"
 
 function App() {
-  
-
   return (
     <>
-      
+      <div>
+        <LightBulb/>
+      </div>
+    </>
+  )
+}
+
+function LightBulb() {
+  const [bulbOn, setbulbOn] = useState(true);
+  return (
+    <>
+    <div>
+      <BulbState bulbOn={bulbOn} />
+      <ToggleBulbState setbulbOn={setbulbOn}/>
+    </div>
+    </>
+  )
+}
+
+function BulbState({ bulbOn }) {
+  return (
+    <>
+    <div>
+      {bulbOn ? "Bulb on" : "Bulb off"}
+    </div>
+    </>
+  )
+}
+
+function ToggleBulbState({ setbulbOn }) {
+  
+  function toggle() {
+    setbulbOn(prevbulbOn => !prevbulbOn);
+  }
+  return (
+    <>
+    <div>
+      <button onClick={toggle}>Toggle the bulb</button>
+    </div>
     </>
   )
 }
