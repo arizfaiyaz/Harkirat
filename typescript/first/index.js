878712ec9c92b456ebb1296e2e7ca7be384f6c5c
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-02-14 22:45:57 +0530

    feat: add greet and add functions with console output in TypeScript

--- typescript/first/index.js
+++ typescript/first/index.js
@@ -2,4 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let x = 10;
 console.log(x);
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+greet("World");
+function add(a, b) {
+    return a + b;
+}
+add(15, 7);
 //# sourceMappingURL=index.js.map
--- typescript/first/index.ts
+++ typescript/first/index.ts
@@ -1,3 +1,15 @@
 let x: number = 10;
 
 console.log(x);
+
+function greet(name : string) {
+    console.log(`Hello ${name}`);
+}
+
+greet("World");
+
+function add(a : number, b: number) {
+    return a + b;
+}
+
+add(15, 7);
