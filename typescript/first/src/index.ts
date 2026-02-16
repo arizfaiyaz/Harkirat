545b465484e2803336556ea6c7ce27d44bf5845e
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-02-16 21:21:44 +0530

    feat: update add function call to log result and add isEven and isLegal functions with user interface

--- typescript/first/src/index.ts
+++ typescript/first/src/index.ts
@@ -12,7 +12,7 @@ function add(a : number, b: number) {
     return a + b;
 }
 
-add(15, 7);
+console.log(add(15, 7));
 
 function isLegalAge(age: number) : boolean {
     if (age >= 18) {
@@ -34,4 +34,47 @@ function greet(name : string){
 
 delayed(() => greet("Alice"));
 
+function isEven(num: number) : boolean {
+    if (num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+console.log(isEven(15));
+
+interface User {
+    name: string,
+    age: number,
+    address: {
+        city: string,
+        country: string,
+        pin: number
+    }
+}
+
+let user : User = {
+    name: "Alice",
+    age: 30,
+    address: { 
+        city: "New York",
+        country: "USA",
+        pin: 12345
+    }
+}
+
+function isLegal(user: User) : boolean {
+    if (user.age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let ans = isLegal(user);
+if (ans) {
+    console.log("User is legal");
+} else {
+    console.log("User is not legal");
+};
