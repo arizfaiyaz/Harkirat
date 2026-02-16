68f13c5b69df7d4cc2f22b7c507c92401524cfd9
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-02-16 13:52:07 +0530

    feat: add initial user greeting examples with comments in TypeScript

---
+++ typescript/first/src/user.ts
@@ -0,0 +1,36 @@
+
+// this is one way to do it
+
+// function greet(user: {
+//     name: string,
+//     age: number
+// }) {
+//     console.log("hello" + user.name);
+//     console.log("Your age is " + user.age);
+    
+// }
+
+// greet({
+//     name: "Ariz",
+//     age: 20
+// });
+
+// other way to do it is like this
+
+// function greet(user: {
+//     name: string,
+//     age: number
+// }) {
+//     console.log("hello " + user.name);
+//     console.log("Your age is " + user.age);
+// }
+
+// let user = {
+//      name: "Alice",
+//      age: 30
+// };
+
+// greet(user);
+
+// another way to do it is like this by using interface
+
