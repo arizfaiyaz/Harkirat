a72aeb6172a751490d449dec4021bee9b964b41d
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-03-14 00:39:48 +0530

    fix: correct User interface definition and comment out unused sumOfAge function

--- typescript/test/src/index.ts
+++ typescript/test/src/index.ts
@@ -1,12 +1,23 @@
 interface User {
     name: string,
-    age: number;
+    age: number,
+    email: string,
+    password: string,
 }
 
-function sumOfAge(user1: User, user2: User) {
-    return user1.age + user2.age;
+// function sumOfAge(user1: User, user2: User) {
+//     return user1.age + user2.age;
+// }
+
+// const age = sumOfAge({name: 'faro', age:20}, {name: 'jiro', age: 30});
+
+// console.log(age);
+
+
+type Updateprops = Pick<User, 'name' | 'age' | 'email'>
+type UpdatePropOptional = Partial<Updateprops>
+function updateUser(updatedProps: UpdatePropOptional){
+
 }
 
-const age = sumOfAge({name: 'faro', age:20}, {name: 'jiro', age: 30});
 
-console.log(age);
